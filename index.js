0a69e6b87ef90fabc9c60ab92853ba733aa3f06c
Marco Fugaro <marco.fugaro@gmail.com>
2021-10-05 19:04:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const unicornFun = (input, {postfix = 'rainbows'} = {}) => {
+	if (typeof input !== 'string') {
+		throw new TypeError(`Expected a string, got ${typeof input}`);
+	}
+
+	return `${input} & ${postfix}`;
+};
+
+export default unicornFun;
