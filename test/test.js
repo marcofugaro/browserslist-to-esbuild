337cd328f49f828cd69a69eb0deea66c185a2c66
Marco Fugaro <marco.fugaro@gmail.com>
2024-01-03 12:34:33 +0100

    👮‍♂️ Update tests

--- test/test.js
+++ test/test.js
@@ -1,29 +1,39 @@
 const path = require('path')
 const test = require('ava')
+const sinon = require('sinon')
 const browserslistToEsbuild = require('../src/index.js')
 
 test('works by passing browsers as array', (t) => {
   const target = browserslistToEsbuild(['>0.2%', 'not dead', 'not op_mini all'])
 
-  t.deepEqual(target, ['chrome79', 'edge92', 'firefox91', 'ios12.2', 'safari13.1'])
+  t.deepEqual(target, ['chrome103', 'edge87', 'firefox115', 'ios12.2', 'safari14.1'])
 })
 
 test('works by passing browsers as string', (t) => {
   const target = browserslistToEsbuild('last 2 versions')
 
-  t.deepEqual(target, ['chrome93', 'edge93', 'firefox92', 'ios14.5', 'safari14.1'])
+  t.deepEqual(target, ['chrome119', 'edge119', 'firefox120', 'ios17', 'safari17.1'])
 })
 
 test('works by loading package.json config', (t) => {
   const packageJsonDir = path.resolve(__dirname, './fixtures')
 
-  process.chdir(packageJsonDir) // makes process.cwd() go in that folder
+  // makes process.cwd() use that folder
+  const cwdStub = sinon.stub(process, 'cwd').returns(packageJsonDir)
 
   process.env.NODE_ENV = 'production'
-  t.deepEqual(browserslistToEsbuild(), ['chrome79', 'edge92', 'firefox91', 'ios12.2', 'safari13.1'])
+  t.deepEqual(browserslistToEsbuild(), [
+    'chrome103',
+    'edge87',
+    'firefox115',
+    'ios12.2',
+    'safari14.1',
+  ])
 
   process.env.NODE_ENV = 'development'
-  t.deepEqual(browserslistToEsbuild(), ['chrome94', 'firefox93', 'safari15'])
+  t.deepEqual(browserslistToEsbuild(), ['chrome120', 'firefox121', 'safari17.2'])
+
+  cwdStub.restore()
 })
 
 test('works with ios', (t) => {
@@ -35,11 +45,11 @@ test('works with ios', (t) => {
 test('works with android and ios', (t) => {
   const target = browserslistToEsbuild('ios >= 11, android >= 5')
 
-  t.deepEqual(target, ['chrome94', 'ios11'])
+  t.deepEqual(target, ['chrome120', 'ios11'])
 })
 
 test('no support for android 4', (t) => {
   const target = browserslistToEsbuild('android >= 4')
 
-  t.deepEqual(target, ['chrome94'])
+  t.deepEqual(target, ['chrome120'])
 })
